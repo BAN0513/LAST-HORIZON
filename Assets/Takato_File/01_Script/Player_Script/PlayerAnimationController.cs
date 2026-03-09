4fb73d5e7933ef3ce3ef0fbf5c7ba8535178eb82
Takato <C4069@oic.jp>
2026-03-09 15:30:25 +0900

    プレイヤーのアニメーションを制御できるようにしたのとコードを少し整理した。

---
+++ Assets/Takato_File/01_Script/Player_Script/PlayerAnimationController.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+/// <summary>
+/// プレイヤーのアニメーションを管理するクラス
+/// </summary>
+public class PlayerAnimationController : MonoBehaviour
+{
+    private Animator animator;
+
+    public bool IsFrontMove { get; private set; }  // プレイヤーが前方に移動しているかどうかを示すフラグ
+    public bool IsLeftMove { get; private set; }  // プレイヤーが左に移動しているかどうかを示すフラグ
+    public bool IsRightMove { get; private set; } // プレイヤーが右に移動しているかどうかを示すフラグ
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>(); // Animator コンポーネントを取得
+    }
+
+    /// <summary>
+    /// アニメーションの状態を更新する
+    /// </summary>
+    public void UpdateAnimation(Vector2 moveInput)
+    {
+        // 入力方向に応じてフラグを設定
+        IsFrontMove = moveInput.y > 0;
+        IsLeftMove = moveInput.x < 0;
+        IsRightMove = moveInput.x > 0;
+
+        // Animator に値を反映
+        animator.SetBool("IsFrontMove", IsFrontMove);
+        animator.SetBool("IsLeftMove", IsLeftMove);
+        animator.SetBool("IsRightMove", IsRightMove);
+    }
+}
--- Assets/Takato_File/01_Script/Player_Script/PlayerController.cs
+++ Assets/Takato_File/01_Script/Player_Script/PlayerController.cs
@@ -10,53 +10,57 @@ namespace Takato
         [Header("(プレイヤー関連のステータス)")]
         [Space(10)]
         [Header("プレイヤーの移動速度")]
-        [SerializeField] private float moveSpeed; // プレイヤーの移動速度
+        [SerializeField] private float moveSpeed;
         [Header("プレイヤーのジャンプ力")]
-        [SerializeField] private float jumpForce; // プレイヤーのジャンプ力
+        [SerializeField] private float jumpForce;
         [Header("プレイヤーの重力")]
-        [SerializeField] private float gravity;   // プレイヤーの重力
+        [SerializeField] private float gravity;
 
-        private PlayerInputController inputController;   // 入力管理クラスへの参照
-        private CharacterController characterController; // キャラクターコントローラーへの参照
+        private PlayerInputController inputController;         // プレイヤーの入力を管理するコンポーネント
+        private CharacterController characterController;       // プレイヤーの移動を管理するコンポーネント
+        private PlayerAnimationController animationController; // プレイヤーのアニメーションを管理するコンポーネント
 
-        private float verticalVelocity; // Y方向の速度を管理する変数
+        private float verticalVelocity; // プレイヤーの垂直方向の速度
 
         private void Awake()
         {
-            inputController = GetComponent<PlayerInputController>();   // 入力管理クラスを取得
-            characterController = GetComponent<CharacterController>(); // キャラクターコントローラーを取得
-        }
-        private void Update()
-        {
-            Move(); // 毎フレームプレイヤーを移動させる関数を呼び出す
+            inputController = GetComponent<PlayerInputController>();   // プレイヤーの入力を管理するコンポーネントを取得
+            characterController = GetComponent<CharacterController>(); // プレイヤーの移動を管理するコンポーネントを取得
+            animationController = GetComponent<PlayerAnimationController>(); // プレイヤーのアニメーションを管理するコンポーネントを取得
         }
 
+        private void Update()
+        {
+            Move(); // プレイヤーの移動とジャンプを処理
+        }
+
+        /// <summary>
+        /// プレイヤーの移動とジャンプを処理するメソッド
+        /// </summary>
         private void Move()
         {
-            // 地面にいる場合はY速度をリセット
             if (characterController.isGrounded)
             {
-                verticalVelocity = -1f; // 少し下向きにして地面に張り付く
+                verticalVelocity = -1f;
                 if (inputController.JumpInput)
                 {
-                    verticalVelocity = jumpForce; // ジャンプ力を加算
+                    verticalVelocity = jumpForce;
                     Debug.Log("ジャンプしました！");
                 }
             }
             else
             {
-                verticalVelocity -= gravity * Time.deltaTime; // 重力を加算
+                verticalVelocity -= gravity * Time.deltaTime;
             }
 
-            // 入力から移動方向を取得
             Vector2 moveDirection = inputController.MoveInput;
             Vector3 movement = new Vector3(moveDirection.x, 0, moveDirection.y) * moveSpeed;
-
-            // Y方向の速度を加算
             movement.y = verticalVelocity;
 
-            // プレイヤーを移動させる
             characterController.Move(movement * Time.deltaTime);
+
+            // アニメーションの更新
+            animationController.UpdateAnimation(moveDirection);
         }
     }
 }
