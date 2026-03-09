bcc812bd3b9c52e20b334009030eceed148f5f74
Takato <C4069@oic.jp>
2026-03-09 16:48:39 +0900

    プレイヤーのジャンプと移動のアニメーションが全部入った
    まだジャンプの処理が少し不安定かもしれないのでまた調整はすると思います。

--- Assets/Takato_File/01_Script/Player_Script/PlayerAnimationController.cs
+++ Assets/Takato_File/01_Script/Player_Script/PlayerAnimationController.cs
@@ -3,32 +3,45 @@ using UnityEngine;
 /// <summary>
 /// プレイヤーのアニメーションを管理するクラス
 /// </summary>
-public class PlayerAnimationController : MonoBehaviour
+namespace Takato
 {
-    private Animator animator;
-
-    public bool IsFrontMove { get; private set; }  // プレイヤーが前方に移動しているかどうかを示すフラグ
-    public bool IsLeftMove { get; private set; }  // プレイヤーが左に移動しているかどうかを示すフラグ
-    public bool IsRightMove { get; private set; } // プレイヤーが右に移動しているかどうかを示すフラグ
-
-    private void Awake()
+    public class PlayerAnimationController : MonoBehaviour
     {
-        animator = GetComponent<Animator>(); // Animator コンポーネントを取得
-    }
+        private Animator animator;
 
-    /// <summary>
-    /// アニメーションの状態を更新する
-    /// </summary>
-    public void UpdateAnimation(Vector2 moveInput)
-    {
-        // 入力方向に応じてフラグを設定
-        IsFrontMove = moveInput.y > 0;
-        IsLeftMove = moveInput.x < 0;
-        IsRightMove = moveInput.x > 0;
+        public bool IsFrontMove { get; private set; }  // プレイヤーが前方に移動しているかどうかを示すフラグ
+        public bool IsLeftMove { get; private set; }  // プレイヤーが左に移動しているかどうかを示すフラグ
+        public bool IsRightMove { get; private set; } // プレイヤーが右に移動しているかどうかを示すフラグ
+        public bool IsBackMove { get; private set; }  // プレイヤーが後方に移動しているかどうかを示すフラグ
+        public bool IsJumpMove { get; set; }      // プレイヤーがジャンプしているかどうかを示すフラグ
 
-        // Animator に値を反映
-        animator.SetBool("IsFrontMove", IsFrontMove);
-        animator.SetBool("IsLeftMove", IsLeftMove);
-        animator.SetBool("IsRightMove", IsRightMove);
+        private void Awake()
+        {
+            animator = GetComponent<Animator>(); // Animator コンポーネントを取得
+        }
+
+        /// <summary>
+        /// アニメーションの状態を更新する
+        /// </summary>
+        public void UpdateAnimation(Vector2 moveInput)
+        {
+            // 入力方向に応じてフラグを設定
+            IsFrontMove = moveInput.y > 0;
+            IsLeftMove = moveInput.x < 0;
+            IsRightMove = moveInput.x > 0;
+            IsBackMove = moveInput.y < 0;
+
+            // Animator に値を反映
+            animator.SetBool("IsFrontMove", IsFrontMove);
+            animator.SetBool("IsLeftMove", IsLeftMove);
+            animator.SetBool("IsRightMove", IsRightMove);
+            animator.SetBool("IsBackMove", IsBackMove);
+        }
+
+        public void SetJump(bool isJump)
+        {
+            IsJumpMove = isJump;
+            animator.SetBool("IsJumpMove", IsJumpMove);
+        }
     }
 }
--- Assets/Takato_File/01_Script/Player_Script/PlayerController.cs
+++ Assets/Takato_File/01_Script/Player_Script/PlayerController.cs
@@ -19,6 +19,7 @@ namespace Takato
         private PlayerInputController inputController;         // プレイヤーの入力を管理するコンポーネント
         private CharacterController characterController;       // プレイヤーの移動を管理するコンポーネント
         private PlayerAnimationController animationController; // プレイヤーのアニメーションを管理するコンポーネント
+        private PlayerAnimationController playerAnimationController; // プレイヤーのアニメーションを管理するコンポーネント
 
         private float verticalVelocity; // プレイヤーの垂直方向の速度
 
@@ -27,6 +28,7 @@ namespace Takato
             inputController = GetComponent<PlayerInputController>();   // プレイヤーの入力を管理するコンポーネントを取得
             characterController = GetComponent<CharacterController>(); // プレイヤーの移動を管理するコンポーネントを取得
             animationController = GetComponent<PlayerAnimationController>(); // プレイヤーのアニメーションを管理するコンポーネントを取得
+            playerAnimationController = GetComponent<PlayerAnimationController>(); // プレイヤーのアニメーションを管理するコンポーネントを取得
         }
 
         private void Update()
@@ -39,28 +41,37 @@ namespace Takato
         /// </summary>
         private void Move()
         {
+            // 地面にいるかどうかをチェックして、ジャンプや重力の処理を行う
             if (characterController.isGrounded)
             {
-                verticalVelocity = -1f;
+                verticalVelocity = -1f; // 地面にいるときは少しだけ下方向に力を加えて、地面にしっかりと接地させる
+
+                // ジャンプ入力があればジャンプする
                 if (inputController.JumpInput)
                 {
                     verticalVelocity = jumpForce;
+                    playerAnimationController.SetJump(true); // ジャンプアニメーションを開始
                     Debug.Log("ジャンプしました！");
+                }
+                else
+                {
+                    playerAnimationController.SetJump(false); // ジャンプアニメーションを終了
                 }
             }
             else
             {
-                verticalVelocity -= gravity * Time.deltaTime;
+                verticalVelocity -= gravity * Time.deltaTime; // 空中にいるときは重力を適用
+                playerAnimationController.SetJump(true); // 空中にいるときはジャンプアニメーションを維持
             }
 
+            // 入力に基づいて移動方向を計算
             Vector2 moveDirection = inputController.MoveInput;
             Vector3 movement = new Vector3(moveDirection.x, 0, moveDirection.y) * moveSpeed;
             movement.y = verticalVelocity;
 
-            characterController.Move(movement * Time.deltaTime);
+            characterController.Move(movement * Time.deltaTime); // プレイヤーを移動させる
 
-            // アニメーションの更新
-            animationController.UpdateAnimation(moveDirection);
+            animationController.UpdateAnimation(moveDirection); // アニメーションの状態を更新
         }
     }
 }
