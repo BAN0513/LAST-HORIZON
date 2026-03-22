6f699f45364dc97916d0ddd27f0ebb250525f82b
Takato <C4069@oic.jp>
2026-03-22 23:23:22 +0900

    プレイヤー用の新たにBlendtree用のAnimationControllerを追加
    移動のアニメーションの滑らかさを上げるために補完のしやすいBlendTreeのアニメーションコントローラーを入れました。前の方のAnimationControllerも一応あります.

--- Assets/Takato_File/01_Script/Player_Script/PlayerAnimationController.cs
+++ Assets/Takato_File/01_Script/Player_Script/PlayerAnimationController.cs
@@ -7,60 +7,63 @@ namespace Takato
 {
     public class PlayerAnimationController : MonoBehaviour
     {
-        private Animator animator; // アニメーターコンポーネント
+        [Header("アニメーションのステータス")]
+        [Space(10)]
+        [Header("アニメーションの補完速度")]
+        [SerializeField] private float animationSmoothTime = 0.1f; // デフォルト値を設定
 
-        public bool IsFrontMove { get; private set; } // 前進アニメーションの状態
-        public bool IsLeftMove { get; private set; } // 左移動アニメーションの状態
-        public bool IsRightMove { get; private set; } // 右移動アニメーションの状態
-        public bool IsBackMove { get; private set; } // 後退アニメーションの状態
-        public bool IsJumpMove { get; private set; } // ジャンプアニメーションの状態
-        public bool IsBlockMove { get; private set; } // ガードアニメーションの状態
-        public bool IsAttackMove { get; private set; } // 攻撃アニメーションの状態
+        private Animator animator;
+
+        // 補完用の現在値・速度
+        private float currentMoveX;
+        private float currentMoveY;
+        private float moveXVelocity;
+        private float moveYVelocity;
+
+        public bool IsFrontMove { get; private set; } // 前方移動の状態を表すプロパティ
+        public bool IsLeftMove { get; private set; }  // 左移動の状態を表すプロパティ
+        public bool IsRightMove { get; private set; } // 右移動の状態を表すプロパティ
+        public bool IsBackMove { get; private set; } // 後方移動の状態を表すプロパティ
+        public bool IsJumpMove { get; private set; } // ジャンプの状態を表すプロパティ
+        public bool IsBlockMove { get; private set; }// 防御の状態を表すプロパティ
+        public bool IsAttackMove { get; private set; }// 攻撃の状態を表すプロパティ
 
         private void Awake()
         {
-            animator = GetComponent<Animator>(); // アニメーターコンポーネントを取得
+            animator = GetComponent<Animator>(); // Animatorコンポーネントを取得
         }
 
         /// <summary>
-        /// 移動アニメーションの状態を更新
+        /// 移動アニメーションの状態を更新（BlendTree対応・補完あり）
         /// </summary>
         public void UpdateAnimation(Vector2 moveInput)
         {
-            // 入力に基づいてアニメーションの状態を更新
+            // 補完してBlendTree用パラメータをセット
+            currentMoveX = Mathf.SmoothDamp(currentMoveX, moveInput.x, ref moveXVelocity, animationSmoothTime);
+            currentMoveY = Mathf.SmoothDamp(currentMoveY, moveInput.y, ref moveYVelocity, animationSmoothTime);
+
+            animator.SetFloat("MoveX", currentMoveX); // BlendTreeのX軸パラメータに補完した値をセット
+            animator.SetFloat("MoveY", currentMoveY); // BlendTreeのY軸パラメータに補完した値をセット
+
+            // 状態プロパティも更新
             IsFrontMove = moveInput.y > 0;
             IsLeftMove = moveInput.x < 0;
             IsRightMove = moveInput.x > 0;
             IsBackMove = moveInput.y < 0;
-
-            // アニメーターのパラメーターを更新
-            animator.SetBool("IsFrontMove", IsFrontMove); 
-            animator.SetBool("IsLeftMove", IsLeftMove);  
-            animator.SetBool("IsRightMove", IsRightMove);
-            animator.SetBool("IsBackMove", IsBackMove);
         }
 
-        /// <summary>
-        /// ジャンプアニメーション
-        /// </summary>
         public void SetJump(bool isJump)
         {
             IsJumpMove = isJump;
             animator.SetBool("IsJumpMove", IsJumpMove);
         }
 
-        /// <summary>
-        /// ガードアニメーション
-        /// </summary>
         public void SetBlock(bool isBlock)
         {
             IsBlockMove = isBlock;
             animator.SetBool("IsBlockMove", IsBlockMove);
         }
 
-        /// <summary>
-        /// 攻撃アニメーション
-        /// </summary>
         public void SetAttack(bool isAttack)
         {
             IsAttackMove = isAttack;
