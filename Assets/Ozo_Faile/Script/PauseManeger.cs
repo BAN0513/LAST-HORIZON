5cd412c4725c287480589ab9c9a8b766543de02c
Haiinu <c5050@oic.jp>
2026-03-26 23:46:43 +0900

    SAVE画像を入れたのとポーズ画面作成

--- Assets/Ozo_Faile/Script/PauseManeger.cs
+++ Assets/Ozo_Faile/Script/PauseManeger.cs
@@ -5,18 +5,24 @@ public class PausManeger : MonoBehaviour
 {
     [Header("ポーズ画面の各UI")]
     public GameObject BackImage;
+    public GameObject BackBrownImage;
+    public GameObject PauseTextImage;
 
     public bool IsPause = false;
 
     public void PauseGame()
     {
         BackImage.SetActive(true);
+        BackBrownImage.SetActive(true);
+        PauseTextImage.SetActive(true);
         Time.timeScale = 0;
     }
 
     public void ResumeGame()
     {
         BackImage.SetActive(false);
+        BackBrownImage.SetActive(false);
+        PauseTextImage.SetActive(false);
         Time.timeScale = 1;
     }
 
